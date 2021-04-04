c8084a437e34bde7a9a9a409b21bfc4a2cd80850
Rolf Krahl <rolf@rotkraut.de>
2021-04-04 11:48:37 +0200

    Add a git_attic() function to call the script in the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
+import os
 from pathlib import Path
+import subprocess
+import sys
 import tarfile
 import pytest
 
 testdir = Path(__file__).resolve().parent
+try:
+    scriptdir = Path(os.environ['BUILD_SCRIPTS_DIR'])
+except KeyError:
+    raise RuntimeError("script directory not set, "
+                       "please use 'setup.py test'") from None
 
 def gettestdata(fname):
     fname = testdir / "data" / fname
@@ -17,3 +25,13 @@ def gitrepo(tmp_path):
     except tarfile.CompressionError as err:
         pytest.skip(str(err))
     return tmp_path / "repo"
+
+def run_cmd(cmd):
+    return subprocess.run(cmd,
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                          check=True, universal_newlines=True)
+
+def git_attic(args):
+    script = scriptdir / "git-attic.py"
+    cmd = [sys.executable, str(script)] + list(args)
+    return run_cmd(cmd)
