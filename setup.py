78abc6f60460cbd2917b8a957465bb56685a4820
Rolf Krahl <rolf@rotkraut.de>
2022-10-08 16:45:17 +0200

    Tested with Python 3.11.0rc2, assume it will work with the Python 3.11 release as well

--- setup.py
+++ setup.py
@@ -102,6 +102,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Version Control :: Git",
     ],
     cmdclass = dict(cmdclass, meta=meta, sdist=sdist),
